9de9835a078300fe9903714f21f6a4e4c64479ec
Paul York <paul@yorkfamily.com>
2019-10-16 11:00:42 -0400

    10-15 stuff

---
+++ 1130-A/oop/main.py
@@ -0,0 +1,16 @@
+from person import Person
+
+# def show_person(p):
+#     print(p.FirstName, p.LastName, p.YearOfBirth)
+
+
+
+p1 = Person()
+#print(p1)
+# show_person(p1)
+p1.show()
+p1.FirstName = 'Paul'
+p1.LastName = 'York'
+p1.YearOfBirth = 1972
+# show_person(p1)
+p1.show()
---
+++ 1130-A/oop/person.py
@@ -0,0 +1,8 @@
+class Person:
+    FirstName = ''
+    LastName = ''
+    YearOfBirth = 1900
+
+    def show(self):
+        print(self.FirstName, self.LastName, self.YearOfBirth)
+
